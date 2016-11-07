9f42abea9e318d71fd84f1184bb33052f299b995
g5191334 <g5191334@gmail.com>
2016-11-07 19:45:47 +0800

    多人連線_大廳

---
+++ Assets/move.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (-1 * speed, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (1 * speed, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (0, 0, 1 * speed);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (0, 0, -1 * speed);
+		}
+
+	}
+}
---
+++ Assets/playercontroller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class playercontroller : NetworkBehaviour {
+	private move playermove;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer)
+		{
+			playermove = GetComponent<move> ();
+			playermove.enabled = false;
+
+		}
+	}
+}
