b37568a49f2bdc3c59da85f728ddc86aa76f2589
g5191334 <g5191334@gmail.com>
2016-11-13 11:09:28 +0800

    人物完成版

--- attack_false.cs
+++ attack_false.cs
@@ -6,5 +6,11 @@ public class attack_false : StateMachineBehaviour {
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
 	{
 		animator.SetBool("attack", false);
+		animator.SetBool ("moveable", false);
+		animator.SetBool ("moveing", false);
 	}
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		animator.SetBool ("moveable", true);
+	}	
 }
--- move.cs
+++ move.cs
@@ -11,50 +11,51 @@ public class move : MonoBehaviour {
 	void Start () {
 		animator = GetComponent<Animator> ();
 		animator.SetBool ("attackable", true);
+		animator.SetBool ("moveable", true);
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKey (KeyCode.A)) {
-			transform.Translate (new Vector3 (-0.1f * speed, 0, 0),Space.World);
-			transform.rotation = Quaternion.Euler(new Vector3(0,-90,0));
-		}
-		if (Input.GetKey (KeyCode.D)) {
-			transform.Translate (new Vector3 (0.1f * speed, 0, 0),Space.World);
-			transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
-		}
-		if (Input.GetKey (KeyCode.W)) {
-			transform.Translate (new Vector3 (0, 0, 0.1f * speed),Space.World);
-			transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
-		}
-		if (Input.GetKey (KeyCode.S)) {
-			transform.Translate (new Vector3 (0, 0, -0.1f * speed),Space.World);
-			transform.rotation = Quaternion.Euler(new Vector3(0,180,0));
-		}
+		if (animator.GetBool ("moveable") == true) {
+			if (Input.GetKey (KeyCode.A)) {
+				transform.Translate (new Vector3 (-0.1f * speed, 0, 0), Space.World);
+				transform.rotation = Quaternion.Euler (new Vector3 (0, -90, 0));
+			}
+			if (Input.GetKey (KeyCode.D)) {
+				transform.Translate (new Vector3 (0.1f * speed, 0, 0), Space.World);
+				transform.rotation = Quaternion.Euler (new Vector3 (0, 90, 0));
+			}
+			if (Input.GetKey (KeyCode.W)) {
+				transform.Translate (new Vector3 (0, 0, 0.1f * speed), Space.World);
+				transform.rotation = Quaternion.Euler (new Vector3 (0, 0, 0));
+			}
+			if (Input.GetKey (KeyCode.S)) {
+				transform.Translate (new Vector3 (0, 0, -0.1f * speed), Space.World);
+				transform.rotation = Quaternion.Euler (new Vector3 (0, 180, 0));
+			}
 
-		if(Input.GetKey (KeyCode.W)&&Input.GetKey (KeyCode.A))
-			transform.rotation = Quaternion.Euler(new Vector3(0,-45,0));
-		else if(Input.GetKey (KeyCode.W)&&Input.GetKey (KeyCode.D))
-			transform.rotation = Quaternion.Euler(new Vector3(0,45,0));
-		else if(Input.GetKey (KeyCode.A)&&Input.GetKey (KeyCode.S))
-			transform.rotation = Quaternion.Euler(new Vector3(0,-135,0));
-		else if(Input.GetKey (KeyCode.D)&&Input.GetKey (KeyCode.S))
-			transform.rotation = Quaternion.Euler(new Vector3(0,135,0));
+			if (Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.A))
+				transform.rotation = Quaternion.Euler (new Vector3 (0, -45, 0));
+			else if (Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.D))
+				transform.rotation = Quaternion.Euler (new Vector3 (0, 45, 0));
+			else if (Input.GetKey (KeyCode.A) && Input.GetKey (KeyCode.S))
+				transform.rotation = Quaternion.Euler (new Vector3 (0, -135, 0));
+			else if (Input.GetKey (KeyCode.D) && Input.GetKey (KeyCode.S))
+				transform.rotation = Quaternion.Euler (new Vector3 (0, 135, 0));
 
-		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.D)) {
-			animator.SetBool ("moveing", true);
-		} else {
-			animator.SetBool ("moveing", false);
+			if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.D)) {
+				animator.SetBool ("moveing", true);
+			} else {
+				animator.SetBool ("moveing", false);
+			}
 		}
-
 
 		if(Input.GetKeyDown(KeyCode.Space)){
 			if (animator.GetBool ("attackable") == true) {
 				animator.SetBool ("attack", true);
 			}
-
-			}
+		}
 
 
 	}
